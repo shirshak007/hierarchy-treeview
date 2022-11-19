efc814275e7970c068b307237f5ef23a70655eb1
Shirshak Das <40979524+shirshak007@users.noreply.github.com>
2022-11-19 18:20:50 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,41 @@
+data = [{
+    "id"    : 1,
+    "name" : "A",
+    "value" : 1.1,
+    "children": [
+        {
+            "id"    : 2,
+            "name" : "B",
+            "value" : "2.1",
+             "children": [
+								{
+								    "id"    : 21,
+								    "name" : "BA",
+								    "value" : "2.1.1",
+								    "children": [
+												{
+														"id"    : 211,
+														"name" : "BAA",
+														"value" : "2.1.1"
+												},
+												{
+														"id"    : 212,
+														"name" : "BAB",
+														"value" : "2.1.2"
+												}
+										]
+								},
+								{
+								    "id"    : 22,
+								    "name" : "BB",
+								    "value" : "2.1.2"
+								}
+						]
+        },
+        {
+            "id"    : 3,
+            "name" : "C",
+            "value" : "2.2"
+        }
+    ]
+}];
